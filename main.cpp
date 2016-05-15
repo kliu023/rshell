fe61405ee4f3f058b76849b42b78b7282d17e68e
henryjyc <jcho043@ucr.edu>
2016-05-15 03:13:04 +0000

    updated main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 #include <string>
 #include "Parsed.cpp"
-// #include "CmdShell.cpp"
+#include "CmdShell.cpp"
 
 using namespace std;
 
-void testParsed(){
-    Parsed parser;
-    string input = "exec1;exec2 arg1 arg2 && exec3 arg3|| exec4 &&exec5 ";
-    cout << "Parsing: " << input << endl;
-    parser.parseInput(input);
-    
-    string temp = "0";
-    while(temp.compare("-1")!=0){
-        temp = parser.getNext();
-        cout << temp << endl;
-    }
+void testCmdShell(){
+    CmdShell cmd;
+    cmd.getCmd();
+    cmd.execute();
 }
 
 int main(){
-    testParsed();
-   
+    testCmdShell();
 }
