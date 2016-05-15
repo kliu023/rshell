006667d047b95af249f73c9ed4cdc75340278799
henryjyc <jcho043@ucr.edu>
2016-05-15 03:05:31 +0000

    updated cmdshell.h

--- CmdShell.h
+++ CmdShell.h
@@ -1,19 +1,23 @@
 #ifndef CMDSHELL_H
 #define CMDSHELL_H
-
+#include <vector>
 #include <string>
+#include "Parsed.h"
+#include "Executable.h"
+#include "Base.h"
+#include "Arguments.h"
 
 using namespace std;
 
 class CmdShell{
     private:
         string input; //user's input 
+        vector<string> cmds;
         Parsed execs; //class name for the parsed
     
     public:
-        bool run(vector <char*> command);
+        bool run(vector<string>);
         void getCmd();//get userinput
         void execute();
-    
 };
 #endif
